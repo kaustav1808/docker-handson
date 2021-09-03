73920bd7084e80e1daf7f2b797ce8721bd36c298
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2021-09-03 07:45:59 +0530

    change.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello world.!!!!!!!!!!!!!!!
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello kaustav..!!!
+        </a>
+      </header>
     </div>
   );
 }
